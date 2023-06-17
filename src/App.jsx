8e0b6c302545463c912f55b3238b23433098a537
Mateo Lopez-Castillo <mateolopcas@gmail.com>
2023-06-16 20:51:03 -0400

    fixed webpack and react browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-// import { Routes, Route } from "react-router-dom"
-// import HomeContainer from "./containers/HomeContainer.jsx"
-// import FavContainer from "./containers/FavContainer.jsx"
-// import LoginContainer from "./containers/LoginContainer.jsx"
-// import SignupContainer from "./containers/SignupContainer.jsx"
-// import Navbar from "./components/Navbar.jsx"
+import { Routes, Route } from "react-router-dom"
+import HomeContainer from "./containers/HomeContainer.jsx"
+import FavContainer from "./containers/FavContainer.jsx"
+import LoginContainer from "./containers/LoginContainer.jsx"
+import SignupContainer from "./containers/SignupContainer.jsx"
+import Navbar from "./components/Navbar.jsx"
 import React from 'react';
 
 
@@ -11,14 +11,14 @@ const App = () => {
   return (
     <div id='appContainer'>
       <h1>This is a test</h1>
-      {/* <Navbar></Navbar> */}
+      <Navbar></Navbar>
       <div id='contentContainer'>
-        {/* <Routes>
+        <Routes>
           <Route exact path='/' element={<HomeContainer></HomeContainer>}></Route>
           <Route path='/favorites' element={<FavContainer></FavContainer>}></Route>
           <Route path='/login' element={<LoginContainer></LoginContainer>}></Route>
           <Route path='/signup' element={<SignupContainer></SignupContainer>}></Route>
-        </Routes> */}
+        </Routes>
       </div>
     </div>
   )
