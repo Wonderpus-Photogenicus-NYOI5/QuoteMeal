2b8697ac56820e93ebbd2847bff2315ccf307b79
Mateo Lopez-Castillo <mateolopcas@gmail.com>
2023-06-17 12:32:58 -0400

    working on favRecipeCard

--- src/containers/FavContainer.jsx
+++ src/containers/FavContainer.jsx
@@ -4,27 +4,30 @@ import FavRecipe from '../components/FavRecipe.jsx'
 import { useSelector, useDispatch } from 'react-redux'
 
 const FavContainer = (props) => {
-    const info = useSelector((state) => state.user.recipes);
-    const username = useSelector((state) => state.username);
-    // const { name, category, region, instructions, image, video, ingredients } = info
-    const recipeList = info.map((el, index) => (
-        <FavRecipe
-            username = {username}
-            index={index}
-            name={el.name}
-            category={el.category}
-            region={el.region}
-            instructions={el.instructions}
-            image={el.image}
-            video={el.video}
-            ingredients={el.ingredients}
-        />
-    ))
-    console.log(recipeList);
-    return <div>
-        <p> Your Favorite Recipes: </p>
+  const info = useSelector((state) => state.user.recipes);
+  const username = useSelector((state) => state.username);
+  // const { name, category, region, instructions, image, video, ingredients } = info
+  const recipeList = info.map((el, index) => (
+    <FavRecipe
+      username={username}
+      index={index}
+      name={el.name}
+      category={el.category}
+      region={el.region}
+      instructions={el.instructions}
+      image={el.image}
+      video={el.video}
+      ingredients={el.ingredients}
+    />
+  ))
+  console.log(recipeList);
+  return (
+    <div>
+      <p className='text-center'> Your Favorite Recipes: </p>
+      <div className='flex flex-row m-10'>
         {recipeList}
-         </div>
+      </div>
+    </div>)
 }
 
 export default FavContainer
