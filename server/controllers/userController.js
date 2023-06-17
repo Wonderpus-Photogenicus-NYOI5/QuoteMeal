858f36c034a8d7701fdd61d03f8f22ce44cb9d73
Isaac Lee <isaaclee@Isaacs-MacBook-Pro.local>
2023-06-16 22:18:40 -0400

    pull time

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,24 +1,35 @@
-<<<<<<< HEAD
-const User = require('./models/User')
-=======
-const User = require('../models/User');
->>>>>>> dev
+const User = require('../models/User')
 const userController = {}
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 
-<<<<<<< HEAD
-userController.createUser = (req, res, next) => {
+userController.createUser = async (req, res, next) => {
     try {
-        const { props } = req.body
-        const newUser = User.create(props)
-        res.locals.newUser = newUser
-        return next()
+        // console.log('body of request', req.body);
+        const { username, password, firstName, lastName, email } = req.body
+        if (username && password) {
+            const salt = await bcrypt.genSaltSync(10)
+            const bcpassword = await bcrypt.hashSync(password, salt)
+            const updatedUser = {
+                username,
+                firstName,
+                lastName,
+                email,
+                password: bcpassword,
+            }
+            const newUser = await User.create(updatedUser)
+            console.log(newUser)
+            res.locals.newUser = newUser
+            // console.log('SUCESSFUL IN CREATE USER, maybe')
+            return next()
+        }
+        next('One of username or password fields is missing')
     } catch (err) {
         next(err)
     }
 }
 
 userController.addFavRecipe = async (req, res, next) => {
+    // console.log('IN FAV RECIPE');
     try {
         const { username, recipe } = req.body
         const userAddFav = await User.findOneAndUpdate(
@@ -31,46 +42,6 @@ userController.addFavRecipe = async (req, res, next) => {
     } catch (err) {
         next(err)
     }
-=======
-
-userController.createUser = async (req, res, next) => {
-  try {
-    // console.log('body of request', req.body);
-    const { username, password, firstName, lastName, email } = req.body;
-    if (username && password) {
-      
-      const salt = await bcrypt.genSaltSync(10);
-      const bcpassword = await bcrypt.hashSync(password, salt);
-      const updatedUser = {username, firstName, lastName, email, password: bcpassword}
-      const newUser = await User.create(updatedUser);
-      console.log(newUser);
-      res.locals.newUser = newUser;
-      // console.log('SUCESSFUL IN CREATE USER, maybe')
-      return next();
-    }
-    next("One of username or password fields is missing");
-  }
-  catch (err) {
-    next(err);
-  }
-}
-
-userController.addFavRecipe = async (req, res, next) => {
-  // console.log('IN FAV RECIPE');
-  try {
-    const { username, recipe } = req.body;
-    const userAddFav = await User.findOneAndUpdate(
-      { username: username },
-      { $push: { favRecipes: recipe } },
-      { new: true }
-    );
-    res.locals.userAddFav = userAddFav;
-    return next()
-  }
-  catch (err) {
-    next(err);
-  }
->>>>>>> dev
 }
 
 userController.deleteRecipe = async (req, res, next) => {
@@ -94,30 +65,24 @@ userController.deleteRecipe = async (req, res, next) => {
 
 userController.login = async (req, res, next) => {}
 
-<<<<<<< HEAD
-module.exports = userController
-=======
 userController.login = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const result = await User.findOne({ username });
-    if (!result) {
-      next("user not found");
+    try {
+        const { username, password } = req.body
+        const result = await User.findOne({ username })
+        if (!result) {
+            next('user not found')
+        }
+        const compare = await bcrypt.compare(password, result.password)
+        // console.log('SUCESSFUL LOG IN');
+        if (compare) {
+            res.locals.user = result
+            return next()
+        } else {
+            next('incorrect password')
+        }
+    } catch (err) {
+        return next(err)
     }
-    const compare = await bcrypt.compare(password, result.password);
-    // console.log('SUCESSFUL LOG IN');
-    if (compare) {
-      res.locals.user = result;
-      return next();
-    }
-    else {
-      next("incorrect password");
-    }
-  }
-  catch (err){
-    return next(err);
-  }
 }
 
-module.exports = userController;
->>>>>>> dev
+module.exports = userController
--- src/store.js
+++ src/store.js
@@ -1,30 +1,14 @@
-<<<<<<< HEAD
 import { configureStore } from '@reduxjs/toolkit'
 import userreducer from './reducers/userreducer.js'
-// import loginreducer from './reducers/loginreducer.js';
+import loginreducer from './reducers/loginreducer.js'
 import testreducer from './reducers/testreducer.js'
 
 const store = configureStore({
     reducer: {
-        // login: loginreducer,
+        login: loginreducer,
         user: userreducer,
         test: testreducer,
     },
 })
-=======
-import { configureStore } from '@reduxjs/toolkit';
-import userreducer from './reducers/userreducer.js';
-import loginreducer from './reducers/loginreducer.js';
-import testreducer from './reducers/testreducer.js';
-
-
-const store = configureStore({
-  reducer: {
-    login: loginreducer,
-    user: userreducer,
-    test: testreducer
-  },
-});
->>>>>>> dev
 
 export default store
